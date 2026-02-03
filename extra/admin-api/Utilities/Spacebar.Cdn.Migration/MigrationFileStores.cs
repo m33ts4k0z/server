fa3c2f99ba3b9ba83980de61a3a77003325e19ce
m33ts4k0z <FF_beatrix12@hotmail.com>
2026-02-03 20:51:11 +0100

    removed c# files

--- extra/admin-api/Utilities/Spacebar.Cdn.Migration/MigrationFileStores.cs
+++
@@ -1,8 +0,0 @@
-using Spacebar.Interop.Cdn.Abstractions;
-
-namespace Spacebar.Cdn.Fsck;
-
-public class MigrationFileStores {
-    public required IFileSource From { get; init; }
-    public required IFileSource To { get; init; }
-}
